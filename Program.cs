ac6111f9df0d232622dd93861149b43acacf98a9
Ekaterina <tebekartina@ya.ru>
2022-08-18 16:32:21 +0400

    есть третье задание

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+        {
+            int a, b, x;
+
+        
+                Console.WriteLine("Введите 3 числа");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                x = Convert.ToInt32(Console.ReadLine());
+                
+                {
+                if (a % 2 == 0) Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
+                }
+                
+                {
+                if (b % 2 == 0) Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
+                }
+
+                {
+                if (x % 2 == 0) Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
+
+                }
+                Console.ReadLine();
+
+        }
